a82f04e662c5c14e0d0a5517435dbbeb1a68ec7e
Ella Jameson <ellagjameson@gmail.com>
2023-12-30 19:04:04 -0700

    Added unittest/doctest integration

---
+++ run_tests.bat
@@ -0,0 +1,26 @@
+@echo off
+
+set ENVNAME=onecondition
+
+set ORIGDIR=%CD%
+set TESTDIR=%ORIGDIR%\tests
+
+call conda activate %ENVNAME%
+
+echo Running doctests...
+python -m unittest discover %TESTDIR%
+if errorlevel 1 goto ERROR
+
+goto DONE
+
+:ERROR
+cd %ORIGDIR%
+call conda deactivate
+echo Tests failed!
+exit /B 1
+
+:DONE
+cd %ORIGDIR%
+call conda deactivate
+echo Tests passed!
+exit /B 0
