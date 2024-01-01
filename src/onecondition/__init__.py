1463bd46d76b1b3eaa8787d3d6a3c1f390b3ff8a
Ella Jameson <ellagjameson@gmail.com>
2024-01-01 04:26:43 -0700

    Update __init__.py

--- src/onecondition/__init__.py
+++ src/onecondition/__init__.py
@@ -9,7 +9,7 @@
     onecondition.ValidationError: Value `0` must be between 0 and 1 (non-inclusive)
 """
 
-__version__ = "1.1.13"
+__version__ = "1.1.14"
 
 __all__ = ["ValidationError", "validate", "test"]
 
